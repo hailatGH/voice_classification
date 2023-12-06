83925c9489b414fc0646c6f57929ebd98dccb32d
Hailemichael Atrsaw <hailat.alx@gmail.com>
2023-12-06 17:29:51 +0300

    test files added

---
+++ client.py
@@ -0,0 +1,13 @@
+import requests
+
+URL = "http://ec2-3-92-141-248.compute-1.amazonaws.com/predict"
+TEST_AUDIO_FILE_PATH = "test/left.wav"
+
+if __name__ == "__main__":
+
+    audio_file = open(TEST_AUDIO_FILE_PATH, "rb")
+    values = {"file": (TEST_AUDIO_FILE_PATH, audio_file, "audio/wav")}
+    response = requests.post(URL, files=values)
+    data = response.json()
+
+    print(f"Predicted keyword is: {data['keyword']}")
